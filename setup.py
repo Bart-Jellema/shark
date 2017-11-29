92b69de9ba7dfd10f8829ccd2c9af4f4df344b14
Bart Jellema <b@rtje.net>
2017-11-30 01:57:13 +0900

    Automatic Commit

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'static/django_markdown/skins/simple/images/*.*',
         'templates/django_markdown/*.*',
     ]},
-    version = "0.3.402",
+    version = "0.3.403",
     description = "Django based bootstrap web framework",
     author = "Bart Jellema",
     author_email = "b@rtje.net",
